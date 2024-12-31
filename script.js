aa5e01f8702be8b207ab901bd6efe4ecf5c23495
enwadiogbu <emekanwadiogbu.jr@me.com>
2024-12-30 19:48:24 -0700

    changed the id: fullscreen to a class to set the addEventListener properly

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
+const speed = document.querySelector('.player-speed');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
-const speed = document.querySelector('.player-speed');
 
 // Play & Pause ----------------------------------- //
 function showPlayIcon() {
